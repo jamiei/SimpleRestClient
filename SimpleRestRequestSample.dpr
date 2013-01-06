36a72e01f5e724c41ceb6457f90aeebb2c113699
Jamie <jamie@op-i.net>
2013-01-06 17:18:38 +0000

    Combined create with fluent request

--- SimpleRestRequestSample.dpr
+++ SimpleRestRequestSample.dpr
@@ -6,14 +6,22 @@ program SimpleRestRequestSample;
 
 uses
   System.SysUtils,
+  Classes,
   RestRequest in 'RestRequest.pas';
 
 var RestReq: TRestRequest;
+    RestResp: THttpResponse;
+    putParams: TStringList;
 
 begin
   try
     try
-      RestReq := TRestRequest.Create();
+      putParams := TStringList.Create();
+      putParams.Add('title=Buy milk');
+      putParams.Add('due-date=01/01/2013 00:00:00');
+      RestReq := TRestRequest.Create().Domain('localhost').Path('todo').WithCredentials('test', 'test');
+      RestResp := RestReq.Put(putParams);
+      if RestResp.ResponseCode = 200 then WriteLn('Your todo was added!');
     finally
       RestReq.Free;
     end;
